77cbb5fc62d7217bd0f6167280eb84da7304328b
RileyDMK <rileydmathis@gmail.com>
2018-02-04 17:49:56 -0800

    Implement lables and navbar

--- src/App.js
+++ src/App.js
@@ -1,5 +1,4 @@
 import React, { Component } from 'react';
-import logo from './images/pivotpointlogo.svg';
 import './App.css';
 import { Switch, Route, Link } from 'react-router-dom';
 
@@ -11,6 +10,7 @@ class App extends Component {
       <div>
         {/* <Header /> */}
         <Main />
+        <Navbar />
       </div>
     );
   }
@@ -51,29 +51,72 @@ const Home = () => (
   </div>
 )
 
+// class NavHole extends Component{
+//   constructor(props){
+//     super(props);
+//     this.state = {isActivated: False};
+//
+//     this.handleHover = this.handlHover.bind(this);
+//   }
+//   handleHover(){
+//     this.setState(prevState => ({
+//       isActivated: !prevState.isActivated
+//     }));
+//   }
+//   render(){
+//     return(
+//       <div></div>
+//     )
+//   }
+// }
+
 const Insight = () => (
-  <div id="insight" className="nav-item">
-    <h1 className="heading">Insight</h1>
-    <div className="hero-text">
+  <div id="insight" className="nav-hole">
+    <h1 id="insight-heading">Insight</h1>
+    {/* <div>
       <Integrity />
       <Integration />
-    </div>
+    </div> */}
   </div>
 )
 const Integrity = () => (
-  <div id="integrity" className="nav-item">
-    <h1>Integrity</h1>
+  <div id="integrity" className="nav-hole">
+    <h1 id="integrity-heading">Integrity</h1>
   </div>
 )
 
-class Integration extends Component {
-  render(){
-    return (
-      <div id="integration" className="nav-item">
-        <h1>Integration</h1>
-      </div>
-    );
-  }
-}
+const Integration = () => (
+  <div id="integration" className="nav-hole">
+    <h1 id="integration-heading">Integration</h1>
+  </div>
+)
+
+const Navbar = () => (
+  <nav className="navbar navbar-expand-lg fixed-bottom navbar-dark bg-*">
+    {/* <a className="navbar-brand" href="#">Riley Mathis</a> */}
+    <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+      <span className="navbar-toggler-icon"></span>
+    </button>
+    <div className="collapse navbar-collapse" id="navbarNav">
+      <ul className="navbar-nav">
+        <li className="nav-item active">
+          <a className="nav-link" href="#">Process<span className="sr-only">(current)</span></a>
+        </li>
+        <li className="nav-item">
+          <a className="nav-link" href="#">Services<span className="sr-only">(current)</span></a>
+        </li>
+        <li className="nav-item">
+          <a className="nav-link" href="#">Portfolio<span className="sr-only">(current)</span></a>
+        </li>
+        <li className="nav-item">
+          <a className="nav-link" href="#">About<span className="sr-only">(current)</span></a>
+        </li>
+      </ul>
+    </div>
+  </nav>
+)
+
+
+
 
 export default App;
