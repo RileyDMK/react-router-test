e90160c64c668c55357b3b7c1d0b7b2056b71651
RileyDMK <rileydmathis@gmail.com>
2018-02-05 16:32:19 -0800

    Implement about page

--- src/App.js
+++ src/App.js
@@ -5,6 +5,14 @@ import { Switch, Route, Link } from 'react-router-dom';
 // TODO: Refactor components into classes so that I can pass down state
 
 class App extends Component {
+  // Constructor(props){
+  //   super(props);
+  //   this.handleAboutUsClick = this.handleAboutUsClick.bind(this);
+  //   this.state = {isAboutUs: false}
+  // }
+  // handleAboutUsClick() {
+  //   this.setState({isAboutUs: true});
+  // }
   render() {
     return (
       <div>
@@ -36,17 +44,22 @@ const Main = () => (
       <Route path='/insight' component={Insight}/>
       <Route path='/integrity' component={Integrity}/>
       <Route path='/integration' component={Integration}/>
+      <Route path='/about' component={AboutUs}/>
     </Switch>
   </main>
 )
 
 const Home = () => (
-  <div className="hero-image">
+  <div>
+
     <div className="main-page">
       <Insight />
       <Integrity />
       <Integration />
       <img src={require("./images/pivotpointlogo_1500.png")} alt="Pivot Point Logo" id="logo" />
+    </div>
+    <div class="hero-image">
+
     </div>
   </div>
 )
@@ -91,6 +104,35 @@ const Integration = () => (
   </div>
 )
 
+const AboutUs = () => (
+  <div>
+    <div class="copy">
+      <img src={require("./images/pivotpointlogo_1500.png")} alt="Pivot Point Logo" id="about-logo" />
+      <p>
+        <br/>
+        Partnerships, Capabilities and References
+        <br/><br/>
+        Our business is your business.
+        <br/>
+        PivotPoint was formed in 2001 with a simple, yet effective, philosophy:
+        relationships with clients are built over time on a foundation of trust.
+        By first focussing on what our clients need and when they need it, we
+        implement a very tailored, fast moving, and, we think, enjoyable approach.
+        The majority of our clients have worked with us, as they have evolved,
+        for an average of five years or longer.
+        <br/>
+        From start-ups to regional and national businesses and non profits,
+        PivotPoint helps you focus on what’s important – your story, your people,
+        your business. Our work is to help you discover and own your story, build
+        passionate corporate culture and position your business advantageously to
+        current and future stakeholders.
+      </p>
+    </div>
+
+    <div class="hero-image"></div>
+  </div>
+)
+
 const Navbar = () => (
   <nav className="navbar navbar-expand-lg fixed-bottom navbar-dark bg-*">
     {/* <a className="navbar-brand" href="#">Riley Mathis</a> */}
@@ -100,10 +142,10 @@ const Navbar = () => (
     <div className="collapse navbar-collapse" id="navbarNav">
       <ul className="navbar-nav">
         <li className="nav-item active">
-          <a className="nav-link" href="#">Process<span className="sr-only">(current)</span></a>
+          <Link to='/' className="nav-link">Process<span className="sr-only">(current)</span></Link>
         </li>
         <li className="nav-item">
-          <a className="nav-link" href="#">Services<span className="sr-only">(current)</span></a>
+          <Link to='/about' className="nav-link">Insight<span className="sr-only">(current)</span></Link>
         </li>
         <li className="nav-item">
           <a className="nav-link" href="#">Portfolio<span className="sr-only">(current)</span></a>
